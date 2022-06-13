235eb6176e4e440e4381118c15e6bec71b0284e8
Robert Gonzalez <robertsgonzalez92@yahoo.com>
2022-06-13 11:54:55 -0500

    df loaded into pgadmin

---
+++ config.py
@@ -0,0 +1,8 @@
+#from sqlalchemy import create_engine
+
+#rds_connection_string = create_engine('postgresql://postgres:gonz4576@localhost:5432/mass_shooting_db')
+
+password = "gonz4576"
+username = "postgres"
+database = "mass_shooting_db"
+
