085b9f2f481026d8d8485e22d223f99afda35f8d
Solomon Rotich <solo@Solomons-MacBook-Pro.local>
2022-06-13 21:46:11 -0400

    word doc

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 
 #rds_connection_string = create_engine('postgresql://postgres:gonz4576@localhost:5432/mass_shooting_db')
 
-password = "gonz4576"
+password = "Kiprop02#"
 username = "postgres"
 database = "mass_shooting_db"
 
