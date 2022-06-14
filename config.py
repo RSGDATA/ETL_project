a8c2e9e036da5a839c752ac03c05a6d3e5c7ae3a
Solomon Rotich <100892067+Srotich1@users.noreply.github.com>
2022-06-14 09:43:51 -0400

    Delete config.py
    disregard

--- config.py
+++
@@ -1,8 +0,0 @@
-#from sqlalchemy import create_engine
-
-#rds_connection_string = create_engine('postgresql://postgres:gonz4576@localhost:5432/mass_shooting_db')
-
-password = "Kiprop02#"
-username = "postgres"
-database = "mass_shooting_db"
-
